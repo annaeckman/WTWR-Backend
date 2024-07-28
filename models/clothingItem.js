82ce92f6f4a971db99c088dd330b1934772399fb
annaeckman <annaeckman@icloud.com>
2024-07-27 21:04:47 -0700

    finish schemas

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -13,8 +13,11 @@ const clothingItemSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
-  owner: { required: true },
-  likes: {},
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true },
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+    default: [],
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
