1b74bd80a847a4f1d0545e1806f069d018606822
annaeckman <annaeckman@icloud.com>
2024-07-27 20:59:15 -0700

    create user and clothing item schema

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ models/clothingItem.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  weather: { type: String, required: true, enum: ["hot", "warm", "cold"] },
+  imageURL: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: { required: true },
+  likes: {},
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("item", clothingItemSchema);
