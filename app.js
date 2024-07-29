d7dffc8f7b69eb7386b7417e8b6cbd1140fb373c
annaeckman <annaeckman@icloud.com>
2024-07-29 09:49:04 -0700

    postman get request bug

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
-const mongoose = require("mongoose");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -10,6 +11,8 @@ mongoose
     console.log("Connected to DB");
   })
   .catch(console.error);
+
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
