f6b3e083eef4e8108312030ab558d239838f7b16
annaeckman <annaeckman@icloud.com>
2024-08-01 09:37:00 -0700

    passed all postman tests

--- controllers/users.js
+++ controllers/users.js
@@ -27,10 +27,13 @@ const getUser = (req, res) => {
   const { userId } = req.params;
 
   User.findById(userId)
+    .orFail()
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "DoesNotExistError") {
         return res.status(404).send({ message: err.message });
       } else if (err.name === "CastError") {
         return res.status(400).send({ message: err.message });
