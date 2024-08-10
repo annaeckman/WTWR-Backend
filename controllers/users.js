f4b33b9058c8cf728392eb96624862fa7f155ee9
annaeckman <annaeckman@icloud.com>
2024-08-10 11:17:33 -0700

    bcrypt bug

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+// const bcrypt = require("bcrypt");
 const {
   BAD_REQUEST,
   NOT_FOUND,
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const itemRouter = require("./clothingItems");
 const userRouter = require("./users");
 const { NOT_FOUND } = require("../utils/errors");
+const { createUser } = require("../controllers/users");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
+
+// app.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
