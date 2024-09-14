964807463c787f455cdbd3692b14aab90093922d
annaeckman <annaeckman@icloud.com>
2024-09-14 15:43:56 -0700

    error handler change

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,12 +1,11 @@
 const errorHandler = (err, req, res, next) => {
-  console.error(err);
-
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({
-    // check the status and display a message based on it
-    message: statusCode === 500 ? "An error occurred on the server" : message,
-  });
+  if (!err.status) {
+    const { statusCode = 500, message } = err;
+    res.status(statusCode).send({
+      // check the status and display a message based on it
+      message: statusCode === 500 ? "An error occurred on the server" : message,
+    });
+  }
 
   console.error(err);
   res.status(err.statusCode).send({ message: err.message });
